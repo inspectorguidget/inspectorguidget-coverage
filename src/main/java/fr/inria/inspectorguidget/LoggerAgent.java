d7079814927a381acf85af73181407c0fdd38402
Ludovic Richoux <ludovic.richoux@inria.fr>
2019-08-19 15:42:40 +0200

    update(Agent) using logger in file

--- src/main/java/fr/inria/inspectorguidget/LoggerAgent.java
+++ src/main/java/fr/inria/inspectorguidget/LoggerAgent.java
@@ -1,14 +1,32 @@
 package fr.inria.inspectorguidget;
 
+import com.sun.jdi.event.ExceptionEvent;
+
+import java.io.IOException;
 import java.lang.instrument.Instrumentation;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class LoggerAgent {
 
     private static Logger LOGGER = Logger.getLogger(LoggerAgent.class.getName());
 
-    public static void premain(String agentArgs, Instrumentation inst) {
-        System.out.println("TESTING AGENT WITH PRINTLN TO SEE IF IT'S LAUNCHED IN TEST PROJECT");
-        //LOGGER.info("[Agent] In premain method");
+    public static void premain(String agentArgs, Instrumentation inst){
+
+        try{
+            FileHandler fh = new FileHandler("AgentLogger.log");
+            fh.setFormatter(new SimpleFormatter());
+            fh.setLevel(Level.ALL);
+            LOGGER.addHandler(fh);
+        } catch (Exception e ){
+            LOGGER.severe("can't log in file");
+        }
+
+        LOGGER.info("[Agent] In premain method");
     }
 }
+
+
+
