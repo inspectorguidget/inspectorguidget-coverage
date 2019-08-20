5c2b978c0e67979e8442dadf4e264515ca48f1f6
Ludovic Richoux <ludovic.richoux@inria.fr>
2019-08-20 15:24:20 +0200

    fix(agent) fixing agent

--- src/main/java/fr/inria/inspectorguidget/BinderTransformer.java
+++ src/main/java/fr/inria/inspectorguidget/BinderTransformer.java
@@ -1,7 +1,7 @@
 package fr.inria.inspectorguidget;
 
 import spoon.decompiler.TypeTransformer;
-import spoon.reflect.code.CtInvocation;
+import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtType;
 
 import java.util.logging.FileHandler;
@@ -19,7 +19,8 @@ public class BinderTransformer implements TypeTransformer {
     @Override
     public boolean accept(CtType type) {
         LOGGER.info("[Agent] checking type : " + type );
-        if ((type instanceof CtInvocation) && type.toString().endsWith("bind()")){
+        if ((type instanceof CtClass) &&
+                type.getMethodsByName("configureBindings").size() > 0) {
             return true;
         } else {
             return false;
@@ -30,7 +31,7 @@ public class BinderTransformer implements TypeTransformer {
     @Override
     public void transform(CtType type) {
         //TODO : add call .log(LogLevel.BINDING) before .bind()
-        LOGGER.info("[Agent] Modifying invocation : " + type );
+        LOGGER.info("[Agent] Modifying invocation : " + type.getSimpleName() );
     }
 }
 
